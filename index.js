32a23170532a94b73d6300f7342969e6ffc487f4
Helen Liutongco <bellpickle@gmail.com>
2017-11-06 04:22:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,4 +3,6 @@ const currentUser="Helen";
 
 const welcomeMessage=`Welcome to Flatbook, ${currentUser}!`;
 
-const excitedWelcomeMessage=`WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`
+const excitedWelcomeMessage=`WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+
+const shortGreeting=`Welcome ${currentUser.slice(1)}!`;
